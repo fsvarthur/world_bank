1782807dec986e916d89f8da4e14d8b748060c3a
fsvarthur <fsvarthur@gmail.com>
2022-12-21 08:02:25 -0300

    x

--- src/components/ListCountries/ListCountries.js
+++ src/components/ListCountries/ListCountries.js
@@ -3,11 +3,11 @@ import { useState, useCallback, useEffect } from 'react';
 import './ListCountries.css';
 import { Link } from 'react-router-dom';
 
-export async function loader({ params }) {
+/*export async function loader({ params }) {
     const response = await fetch(`https://api.worldbank.org/v2/country/${params.id}?format=json`);
     const data = await response.json();
     return data;
-}
+}*/
 
 
 export default function ListCountries(){
